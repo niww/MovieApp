7c4d319421ef6e9b16ee9098ae2cbf3b02c65e9a
niww <niww@ya.ru>
2021-02-11 23:48:49 +0300

    Initial commit

---
+++ app/src/main/java/com/github/niww/movieapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.github.niww.movieapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.bottom).setOnClickListener {
+            val intent = Intent(this, MovieDetailsActivity::class.java)
+            this.startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/github/niww/movieapp/MovieDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.github.niww.movieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
